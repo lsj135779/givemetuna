6bd42550f333e51c38f601a88a23019832adcd32
이승준 <lsj135779@gmail.com>
2023-12-26 21:24:17 +0900

    git init

---
+++ src/main/java/com/sparta/givemetuna/GivemetunaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.givemetuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivemetunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivemetunaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/givemetuna/GivemetunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.givemetuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivemetunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
