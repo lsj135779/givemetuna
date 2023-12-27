e8158bac923f12efec3bdbee8eb862bdc84ab006
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-12-28 06:49:22 +0900

    feat : Stage와 Card 간 연관관계 추가

--- src/main/java/com/sparta/givemetuna/domain/card/entity/Card.java
+++ src/main/java/com/sparta/givemetuna/domain/card/entity/Card.java
@@ -1,12 +1,15 @@
 package com.sparta.givemetuna.domain.card.entity;
 
 import com.sparta.givemetuna.domain.checklist.entity.Checklist;
+import com.sparta.givemetuna.domain.column.entity.Stage;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.sql.Timestamp;
@@ -16,6 +19,15 @@ import java.util.List;
 @Entity
 @Table(name = "card")
 public class Card {
+
+	@OneToMany(mappedBy = "card", targetEntity = UserCard.class, cascade = CascadeType.ALL, orphanRemoval = true)
+	private final List<UserCard> userCards = new ArrayList<>();
+
+	@OneToMany(mappedBy = "card", targetEntity = Checklist.class, cascade = CascadeType.ALL, orphanRemoval = true)
+	private final List<Checklist> checklists = new ArrayList<>();
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Stage stage;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,10 +47,4 @@ public class Card {
 
 	@Column
 	private Timestamp closedAt;
-
-	@OneToMany(mappedBy = "card", targetEntity = UserCard.class, cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<UserCard> userCards = new ArrayList<>();
-
-	@OneToMany(mappedBy = "card", targetEntity = Checklist.class, cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Checklist> checklists = new ArrayList<>();
 }
