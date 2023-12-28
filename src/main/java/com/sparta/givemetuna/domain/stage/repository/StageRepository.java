90af9025d764772d0f48d2edad6c935f8fedf0cc
Yoosup Shin <brian96shin@gmail.com>
2023-12-28 20:56:20 +0900

    feat: Board Controller CRUD 1차 완료

--- src/main/java/com/sparta/givemetuna/domain/stage/repository/StageRepository.java
+++ src/main/java/com/sparta/givemetuna/domain/stage/repository/StageRepository.java
@@ -1,6 +1,6 @@
 package com.sparta.givemetuna.domain.stage.repository;
 
-import com.sparta.givemetuna.domain.column.entity.Stage;
+import com.sparta.givemetuna.domain.stage.entity.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StageRepository extends JpaRepository<Stage, Long> {
