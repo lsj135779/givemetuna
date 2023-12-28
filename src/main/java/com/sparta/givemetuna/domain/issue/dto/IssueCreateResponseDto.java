6ac08d042489d0a2a7faebcf81d4ba2a96110b9c
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-12-28 09:24:55 +0900

    feat: Issue 요청/응답 dto

---
+++ src/main/java/com/sparta/givemetuna/domain/issue/dto/IssueCreateResponseDto.java
@@ -0,0 +1,31 @@
+package com.sparta.givemetuna.domain.issue.dto;
+
+import com.sparta.givemetuna.domain.issue.entity.Issue;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter
+public class IssueCreateResponseDto {
+
+	private String title;
+
+	private String contents;
+
+	private long cardId;
+
+	private LocalDateTime createdAt;
+
+	public static IssueCreateResponseDto of(Issue issue) {
+		return new IssueCreateResponseDto(
+			issue.getTitle(),
+			issue.getContents(),
+			issue.getCard().getId(),
+			issue.getCreatedAt()
+		);
+	}
+}
