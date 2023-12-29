db756d55e458602d67e4fcece1fda677a9abd620
이승준 <lsj135779@gmail.com>
2023-12-29 19:05:41 +0900

    충돌방지를 위해 temp에서 boardService 수행

--- src/main/java/com/sparta/givemetuna/domain/checklist/temp/cardTemp/CardTempService.java
+++ src/main/java/com/sparta/givemetuna/domain/checklist/temp/cardTemp/CardTempService.java
@@ -1,4 +1,4 @@
-package com.sparta.givemetuna.domain.checklist.temp.card.service;
+package com.sparta.givemetuna.domain.checklist.temp.cardTemp;
 
 import com.sparta.givemetuna.domain.card.entity.Card;
 import com.sparta.givemetuna.domain.card.repository.CardRepository;
@@ -12,7 +12,7 @@ public class CardTempService {
 	private final CardRepository cardRepository;
 
 	public Card getCard(Long cardId) {
-		Card card = cardRepository.findById(cardId).orElseThrow(() -> new IllegalArgumentException("없음"));
+		Card card = cardRepository.findById(cardId).orElseThrow(() -> new IllegalArgumentException("해당하는 카드정보가 없습니다."));
 		return card;
 	}
 }
