a01870a114bed3f2dc7124f66c16fe3810abb884
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-01-02 06:36:30 +0900

    feat: {계정:권한} 입력에 따른 사용자 초대 API 구현

--- src/main/java/com/sparta/givemetuna/domain/board/entity/Board.java
+++ src/main/java/com/sparta/givemetuna/domain/board/entity/Board.java
@@ -4,18 +4,33 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sparta.givemetuna.domain.board.dto.CreateBoardRequestDto;
 import com.sparta.givemetuna.domain.stage.entity.Stage;
 import com.sparta.givemetuna.domain.user.entity.BoardUserRole;
+import com.sparta.givemetuna.domain.user.entity.Role;
 import com.sparta.givemetuna.domain.user.entity.User;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
-@NoArgsConstructor
 @Table(name = "board")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
 
 	@OneToMany(mappedBy = "board", targetEntity = Stage.class, cascade = CascadeType.ALL, orphanRemoval = true)
@@ -40,10 +55,24 @@ public class Board {
 		this.name = requestDto.getName();
 	}
 
-	public void setUser(User user) { this.user = user; }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public void addUsersWithRole(Map<User, Role> usersWithRole) {
+		for (User user : usersWithRole.keySet()) {
+			addUserWithRole(user, usersWithRole.get(user));
+		}
+	}
+
+	private void addUserWithRole(User user, Role role) {
+		BoardUserRole boardUserRole = new BoardUserRole(user, role, this);
+		invitedUserRole.add(boardUserRole);
+		user.getBoardUserRoles().add(boardUserRole);
 	}
 }
--- src/main/java/com/sparta/givemetuna/domain/user/entity/BoardUserRole.java
+++ src/main/java/com/sparta/givemetuna/domain/user/entity/BoardUserRole.java
@@ -11,25 +11,35 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "boardUserRole")
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardUserRole {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "board_id")
-    private Board board;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "board_id")
+	private Board board;
+
+	public BoardUserRole(User user, Role role, Board board) {
+		this.user = user;
+		this.role = role;
+		this.board = board;
+	}
 }
