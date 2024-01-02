66b33c393e715764eeff589e97d43bd795255f0e
Yoosup Shin <brian96shin@gmail.com>
2024-01-02 15:40:32 +0900

    feat: board, stage 기능 완성 stage 페이징 기능 추가 필요 board, stage 권한 확인 추가 필요

--- src/main/java/com/sparta/givemetuna/domain/board/controller/BoardController.java
+++ src/main/java/com/sparta/givemetuna/domain/board/controller/BoardController.java
@@ -1,30 +1,15 @@
 package com.sparta.givemetuna.domain.board.controller;
 
-import com.sparta.givemetuna.domain.board.dto.BoardListResponseDto;
-import com.sparta.givemetuna.domain.board.dto.BoardResponseDto;
-import com.sparta.givemetuna.domain.board.dto.CreateBoardRequestDto;
-import com.sparta.givemetuna.domain.board.dto.CreateBoardResponseDto;
-import com.sparta.givemetuna.domain.board.dto.DeleteBoardResponseDto;
-import com.sparta.givemetuna.domain.board.dto.InviteUserRequestDto;
-import com.sparta.givemetuna.domain.board.dto.UpdateBoardRequestDto;
-import com.sparta.givemetuna.domain.board.dto.UpdateBoardResponseDto;
+import com.sparta.givemetuna.domain.board.dto.*;
 import com.sparta.givemetuna.domain.board.entity.Board;
 import com.sparta.givemetuna.domain.board.service.BoardService;
-import java.util.List;
-
 import com.sparta.givemetuna.domain.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,7 +24,6 @@ public class BoardController {
 	public ResponseEntity<CreateBoardResponseDto> createBoard(@RequestBody CreateBoardRequestDto requestDto,
 															  @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		CreateBoardResponseDto responseDto = boardService.createBoard(requestDto, userDetails.getUser());
-
 		return ResponseEntity.ok().body(responseDto);
 	}
 
@@ -61,7 +45,7 @@ public class BoardController {
 	// todo: stage와 card까지 보여져야함
 	// todo:생성한 사람과 초대받은 사람 이외의 사람들에겐 접근권한 없어야함
 	@GetMapping("/{boardId}")
-	public ResponseEntity<BoardResponseDto> getBoard(@PathVariable Long boardId) {
+	public ResponseEntity<BoardResponseDto> getBoard(@PathVariable(name = "boardId") Long boardId) {
 		Board board = boardService.getBoard(boardId);
 		BoardResponseDto responseDto = BoardResponseDto.of(board);
 		return ResponseEntity.ok().body(responseDto);
@@ -70,7 +54,7 @@ public class BoardController {
 	// board 삭제
 	// todo: user 권한 확인 필요
 	@DeleteMapping("/{boardId}")
-	public ResponseEntity<DeleteBoardResponseDto> deleteBoard(@PathVariable Long boardId,
+	public ResponseEntity<DeleteBoardResponseDto> deleteBoard(@PathVariable(name = "boardId") Long boardId,
 															  @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		boardService.deleteBoard(boardId, userDetails.getUser());
 		return ResponseEntity.ok().body(new DeleteBoardResponseDto("Board deleted successfully"));
@@ -79,7 +63,7 @@ public class BoardController {
 	// board 수정
 	// TODO: 2023-12-29 user 권한 확인 기능 필요 
 	@PatchMapping("/{boardId}")
-	public ResponseEntity<UpdateBoardResponseDto> updateBoard(@PathVariable Long boardId,
+	public ResponseEntity<UpdateBoardResponseDto> updateBoard(@PathVariable(name = "boardId") Long boardId,
 															  @RequestBody UpdateBoardRequestDto requestDto,
 															  @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		Board board = boardService.updateBoard(boardId, requestDto, userDetails.getUser());
--- src/main/java/com/sparta/givemetuna/domain/board/service/BoardService.java
+++ src/main/java/com/sparta/givemetuna/domain/board/service/BoardService.java
@@ -10,119 +10,113 @@ import com.sparta.givemetuna.domain.stage.entity.Stage;
 import com.sparta.givemetuna.domain.stage.repository.StageRepository;
 import com.sparta.givemetuna.domain.user.entity.Role;
 import com.sparta.givemetuna.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class BoardService {
 
-	private final BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
 
-	private final StageRepository stageRepository;
+    private final StageRepository stageRepository;
 
-	// board 생성
-	public CreateBoardResponseDto createBoard(CreateBoardRequestDto requestDto, User user) {
-		// board 만들기
-		Board board = new Board(requestDto);
+    // board 생성
+    public CreateBoardResponseDto createBoard(CreateBoardRequestDto requestDto, User user) {
+        // board 만들기
+        Board board = new Board(requestDto);
 
-		// ***board에 user 넣기
+        // board에 user 넣기
         board.setUser(user);
 
-		// board에 user 권한 총책임자 설정
-		// board.setBoardRole("총책임자");
+        // board에 user 권한 총책임자 설정
+//        setBoardManager(board, user);
 
-		// 4가지 stage 만들어서 넣기
-		createDefaultStages(board, user);
+        // 4가지 stage 만들어서 넣기
+        createDefaultStages(board, user);
 
-		// board 저장
-		boardRepository.save(board);
+        // board 저장
+        boardRepository.save(board);
 
 
-		return new CreateBoardResponseDto(board);
-	}
+        return new CreateBoardResponseDto(board);
+    }
 
-	// user 검증 추가 필요
-	public void deleteBoard(Long boardId, User user) {
-		//유저 권한 체크
-		Board board = checkBoardAuth(boardId, user);
+    // user 검증 추가 필요
+    public void deleteBoard(Long boardId, User user) {
+        //유저 권한 체크
+        Board board = checkBoardAuth(boardId, user);
 
-		boardRepository.delete(board);
-	}
+        boardRepository.delete(board);
+    }
 
-	// 단일 board 찾기
-	@Transactional(readOnly = true)
-	public Board getBoard(Long boardId) {
-		Board board = boardRepository.findById(boardId)
-			.orElseThrow(() -> new IllegalArgumentException("The board does not exist"));
-		return board;
-	}
+    // 단일 board 찾기
+    @Transactional(readOnly = true)
+    public Board getBoard(Long boardId) {
+        return boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("The board does not exist"));
+    }
 
-	// 모든 board 찾기
-	@Transactional(readOnly = true)
-	public List<BoardListResponseDto> getAllBoards() {
-		List<Board> boardList = boardRepository.findAll();
-		return boardList.stream()
-			.map(board -> new BoardListResponseDto(board.getName()))
-			.collect(Collectors.toList());
-	}
+    // 모든 board 찾기
+    @Transactional(readOnly = true)
+    public List<BoardListResponseDto> getAllBoards() {
+        List<Board> boardList = boardRepository.findAll();
+        return boardList.stream()
+                .map(board -> new BoardListResponseDto(board.getName()))
+                .collect(Collectors.toList());
+    }
 
-	// update
-	@Transactional
-	public Board updateBoard(Long boardId, UpdateBoardRequestDto requestDto, User user) {
-		// user 검증 로직
-		Board board = checkBoardAuth(boardId, user);
+    // update
+    @Transactional
+    public Board updateBoard(Long boardId, UpdateBoardRequestDto requestDto, User user) {
+        // user 검증 로직
+        Board board = checkBoardAuth(boardId, user);
 
-		board.setName(requestDto.getName());
-		return board;
-	}
+        board.setName(requestDto.getName());
+        return board;
+    }
 
-	// Board 작성자 검증
-	private Board checkBoardAuth(Long id, User user) {
-		// id로 보드를 찾은후
-		Board board = getBoardById(id);
-		// user의 id와 대조
-		if (!user.getId().equals(board.getUser().getId())) {
-			throw new RejectedExecutionException("총 책임자만 접근 가능합니다.");
-		}
-		return board;
-	}
+    // Board 작성자 검증
+    private Board checkBoardAuth(Long id, User user) {
+        // id로 보드를 찾은후
+        Board board = getBoard(id);
+        // user의 id와 대조
+        if (!user.getId().equals(board.getUser().getId())) {
+            throw new RejectedExecutionException("총 책임자만 접근 가능합니다.");
+        }
+        return board;
+    }
 
-	// Board id로 board 찾기
-	@Transactional(readOnly = true)
-	public Board getBoardById(Long id) {
-		return boardRepository.findById(id)
-			.orElseThrow(() -> new IllegalArgumentException("The board does not exist"));
-	}
+    // board 총 책임자 권한 설정
+//    private void setBoardManager(Board board, User user) {
+//        BoardUserRole boardUserRole = new BoardUserRole(user, Role.GENERAL_MANAGER, board);
+//        user.getBoardUserRoles().add(boardUserRole);
+//    }
 
-	// board 총 책임자 권한 설정
-	private void setUserBoardManager(User user) {
-	}
+    public Board inviteUser(Long boardId, Map<User, Role> users) {
+        Board board = getBoard(boardId);
+        board.addUsersWithRole(users);
+        return board;
+    }
 
-	public Board inviteUser(Long boardId, Map<User, Role> users) {
-		Board board = getBoard(boardId);
-		board.addUsersWithRole(users);
-		return board;
-	}
+    // 기본 Stage 생성 메서드
+    @Transactional
+    public void createDefaultStages(Board board, User user) {
+        List<Stage> defaultStage = new ArrayList<>();
 
-	// 기본 Stage 생성 메서드
-	@Transactional
-	public void createDefaultStages(Board board, User user) {
-		List<Stage> defaultStage = new ArrayList<>();
+        defaultStage.add(new Stage(board, "백로그", user));
+        defaultStage.add(new Stage(board, "프로세스", user));
+        defaultStage.add(new Stage(board, "완료", user));
+        defaultStage.add(new Stage(board, "비상", user));
 
-		defaultStage.add(new Stage(board, "백로그", user));
-		defaultStage.add(new Stage(board, "프로세스", user));
-		defaultStage.add(new Stage(board, "완료", user));
-		defaultStage.add(new Stage(board, "비상", user));
-
-		stageRepository.saveAll(defaultStage);
-	}
+        stageRepository.saveAll(defaultStage);
+    }
 }
